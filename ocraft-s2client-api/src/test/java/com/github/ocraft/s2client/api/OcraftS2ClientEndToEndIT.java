27559be1cf4d81cba8e92ad2a079f8e48dcf2d2e
Piotr Picheta <ocraftproject@gmail.com>
2017-11-17 19:08:56 +0100

    Updated README file.

--- ocraft-s2client-api/src/test/java/com/github/ocraft/s2client/api/OcraftS2ClientEndToEndIT.java
+++ ocraft-s2client-api/src/test/java/com/github/ocraft/s2client/api/OcraftS2ClientEndToEndIT.java
@@ -1,18 +1,18 @@
 /**
  * The MIT License (MIT)
- *
+ * <p>
  * Copyright (c) 2017, Ocraft Project
- *
+ * <p>
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
+ * <p>
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- *
+ * <p>
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -67,7 +67,6 @@ import static com.github.ocraft.s2client.protocol.game.PlayerSetup.participant;
 import static com.github.ocraft.s2client.protocol.game.Race.*;
 import static com.github.ocraft.s2client.protocol.query.Queries.*;
 import static com.github.ocraft.s2client.protocol.request.RequestData.Type.*;
-import static com.github.ocraft.s2client.protocol.request.RequestObserverAction.observerActions;
 import static com.github.ocraft.s2client.protocol.request.Requests.*;
 
 class OcraftS2ClientEndToEndIT {
