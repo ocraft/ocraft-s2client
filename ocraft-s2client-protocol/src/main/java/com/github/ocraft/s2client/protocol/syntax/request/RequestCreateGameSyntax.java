de67759b433e61d24f6ce77751377066821bf61e
Piotr Picheta <ocraftproject@gmail.com>
2018-07-01 13:20:59 +0200

    Updated license plugin.

--- ocraft-s2client-protocol/src/main/java/com/github/ocraft/s2client/protocol/syntax/request/RequestCreateGameSyntax.java
+++ ocraft-s2client-protocol/src/main/java/com/github/ocraft/s2client/protocol/syntax/request/RequestCreateGameSyntax.java
@@ -1,18 +1,21 @@
-/**
- * The MIT License (MIT)
- *
- * Copyright (c) 2017, Ocraft Project
- *
+package com.github.ocraft.s2client.protocol.syntax.request;
+
+/*-
+ * #%L
+ * ocraft-s2client-protocol
+ * %%
+ * Copyright (C) 2017 - 2018 Ocraft Project
+ * %%
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
+ * 
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- *
+ * 
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -20,8 +23,8 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
+ * #L%
  */
-package com.github.ocraft.s2client.protocol.syntax.request;
 
 import com.github.ocraft.s2client.protocol.game.BattlenetMap;
 import com.github.ocraft.s2client.protocol.game.LocalMap;
