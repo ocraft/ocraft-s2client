edbc5cdce3632a32d877b3470ef2b5c6b0dc42b9
Piotr Picheta <ocraftproject@gmail.com>
2024-01-20 19:13:09 +0100

    revert problematic changes

--- ocraft-s2client-bot/src/main/java/com/github/ocraft/s2client/bot/S2ReplayObserver.java
+++ ocraft-s2client-bot/src/main/java/com/github/ocraft/s2client/bot/S2ReplayObserver.java
@@ -60,7 +60,7 @@ public class S2ReplayObserver extends Client {
      * @param replayInfo Replay information used to decide if the replay should be filtered.
      * @return If 'true', the replay will be rejected and not analyzed.
      */
-    public boolean ignoreReplay(ReplayInfo replayInfo, int playerId) {
+    boolean ignoreReplay(ReplayInfo replayInfo, int playerId) {
         return replayInfo.getGameDurationSeconds() < MINIMUM_REPLAY_DURATION;
     }
 
